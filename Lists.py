8367677d14407c6442f225cf9f53454f8f506ba1
Sweta Sharan <sweta.sharan143@gmail.com>
2026-09-22 13:47:34 +0200

    lists of fruits

---
+++ Lists.py
@@ -0,0 +1,11 @@
+fruits=["apple","banana","orange","strawberry"]
+#Print the first fruit
+print(fruits[0])
+print(fruits[-1])
+#Add "mango" to the list
+fruits.append("mango")
+print(fruits)
+#print the number of fruits in the list
+print(len(fruits))
+#print the final list of fruits
+print(fruits)
